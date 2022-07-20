9671ff08e995ce113a7781d5d10e6f1f7bf795a5
Htet Wai Aung <htetwaiaung1236@gmail.com>
2022-07-21 02:12:59 +0630

    excel and pdf download

--- src/main/java/com/ojt/mvc_student_jpa/MvcStudentJpaApplication.java
+++ src/main/java/com/ojt/mvc_student_jpa/MvcStudentJpaApplication.java
@@ -1,11 +1,21 @@
 package com.ojt.mvc_student_jpa;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.View;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +26,15 @@ import com.ojt.mvc_student_jpa.service.ReportService;
 
 import net.sf.jasperreports.engine.JRException;
 
+
 @SpringBootApplication
-@RestController
+@Controller
 public class MvcStudentJpaApplication {
 	@Autowired
 	private StudentRepo studentRepo;
+
+	@Autowired 
+	private ServletContext context;
 
 	@Autowired
 	private ReportService reportService;
@@ -31,9 +45,38 @@ public class MvcStudentJpaApplication {
 	}
 
 	@GetMapping("/report/{format}")
-	public String generateReport(@PathVariable String format) throws FileNotFoundException, JRException{
-		return reportService.exportReport(format);
+	public void generateReport(HttpServletResponse response,HttpServletRequest request,@PathVariable String format) throws JRException, IOException{
+		 reportService.exportReport(format,response);
+		
+		
 	}
+
+	// private void fileDownload(String fullPath,HttpServletResponse response,String filename) throws IOException{
+	// 	File  file = new File(fullPath);
+	// 	final int BUFFER_SIZE = 4096;
+	// 	if(file.exists()){
+	// 		try {
+	// 			FileInputStream inputStream = new FileInputStream(file);
+	// 			String mineType = context.getMimeType(fullPath);
+	// 			response.setContentType(mineType);
+	// 			response.setHeader("content-disposition", "attachment; filename = " + filename);
+	// 			OutputStream outputStream = response.getOutputStream();
+	// 			byte[] buffer = new byte[BUFFER_SIZE];
+	// 			int byteRead = -1;
+	// 			while((byteRead = inputStream.read(buffer))!= -1){
+	// 				outputStream.write(buffer,0,byteRead);
+	// 			}
+	// 			inputStream.close();
+	// 			outputStream.close();
+	// 			file.delete();
+
+	// 		} catch (FileNotFoundException e) {
+	// 			// TODO Auto-generated catch block
+	// 			e.printStackTrace();
+	// 		}
+			
+	// 	}
+	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcStudentJpaApplication.class, args);
--- src/main/java/com/ojt/mvc_student_jpa/service/ReportService.java
+++ src/main/java/com/ojt/mvc_student_jpa/service/ReportService.java
@@ -2,9 +2,12 @@ package com.ojt.mvc_student_jpa.service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +16,6 @@ import org.springframework.util.ResourceUtils;
 import com.ojt.mvc_student_jpa.model.Student;
 import com.ojt.mvc_student_jpa.repo.StudentRepo;
 
-import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -21,15 +23,21 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
 
 @Service
 public class ReportService {
     @Autowired
     private StudentRepo studentRepo;
 
-    public String exportReport(String   reportFormat) throws FileNotFoundException, JRException{
-        String path = "C:\\Users\\DELL\\Desktop\\javaEE-workspace\\OJT_Mvc_SpringBoot_JPA\\src\\main\\resources";
+    public void exportReport(String   reportFormat,HttpServletResponse response) throws JRException, IOException{
+        String path = "C:\\Users\\Admin\\eclipse-workspace\\OJT_Mvc_SpringBoot_JPA\\src\\main\\resources";
         List<Student> student = studentRepo.findAll();
         File file = ResourceUtils.getFile("classpath:student.jrxml");
         JasperReport jasperReport =JasperCompileManager.compileReport(file.getAbsolutePath());
@@ -37,12 +45,33 @@ public class ReportService {
         Map<String,Object> parameter = new HashMap<>();
         parameter.put("createBy", "Java Techie");
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameter, dataSource); 
+
         if(reportFormat.equalsIgnoreCase("html")){
+
             JasperExportManager.exportReportToHtmlFile(jasperPrint,path+"\\student.html");
         }
-        if(reportFormat.equalsIgnoreCase("pdf")){
-            JasperExportManager.exportReportToPdfFile(jasperPrint,path+"\\student.pdf");
+        else if(reportFormat.equalsIgnoreCase("pdf")){
+            /* JRPdfExporter exporter = new JRPdfExporter();
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            exporter.setExporterOutput(response.getOutputStream()); */
+            JasperExportManager.exportReportToPdfStream(jasperPrint,response.getOutputStream());
+            response.setContentType("application/pdf");
+            response.addHeader("Content-Disposition", "inline; filename=student.pdf;");
+            
         }
-        return "report generate in path : " + path;
+        else if(reportFormat.equalsIgnoreCase("excel")){
+            JRXlsxExporter exporter = new JRXlsxExporter(); // initialize exporter 
+          exporter.setExporterInput(new SimpleExporterInput(jasperPrint)); // set compiled report as input
+          
+          SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+          configuration.setOnePagePerSheet(true); // setup configuration
+          configuration.setDetectCellType(true);
+          exporter.setConfiguration(configuration); 
+          exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream())); 
+          response.setContentType("application/xlsx");
+            response.addHeader("Content-Disposition", "inline; filename=student.xlsx;"); 
+          exporter.exportReport();
+        }
+        
     }
 }
